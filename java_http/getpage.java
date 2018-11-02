6ed2a744d0c5d4a7ced42d6a79485489724a440b
Kaveh Pezeshki <kaveh@WL-197-188.WPA.Claremont.Edu>
2018-11-02 14:01:48 -0700

    added flight parsing

--- java_http/getpage.java
+++ java_http/getpage.java
@@ -19,7 +19,7 @@ public class getpage {
 
         String line = "";
         while ((line = s.readLine()) != null) {
-            textFromWebpage += line;
+            textFromWebpage += line + "\n";
         }
         s.close();
         return textFromWebpage;
@@ -32,20 +32,57 @@ public class getpage {
         [Flight, Alt, Speed, Heading, Lat, Long, Sig, Msgs],
         ...
     ]
-    */
-    /* CURRENTLY NONFUNCTIONAL
-    public String[][] getFlights(String webPage) {
-        String[] lines = webpage.split("\n");
-        //ignoring the first and second lines, creating a new string array for every line after the two headers
-        for(int i = 2, i < lines.length(), i++ {
 
-        }
-    }
+    Example input flight data:
+Hex     Mode  Sqwk  Flight   Alt    Spd  Hdg    Lat      Long   Sig  Msgs   Ti/
+-------------------------------------------------------------------------------
+0D0A78  S     3310           35000  492  145   34.220 -117.774    6    89    2
+A5C6A1  S     7322  UPS966    9575  294  308   34.113 -117.835    6   391    1
     */
+    
+    public static String[][] getFlights(String webPage) {
+        String[] lines = webPage.split("\n");
+        //System.out.println(lines);
+        String[][] planeData = new String[lines.length-2][8];
+        //ignoring the first and second lines, creating a new string array for every line after the two headers
+        //TODO: rewrite after we have a better idea of string parsing limits. Don't want to miss a digit if fields have different sizes than expected
+        System.out.println("lines");
+        for(int i = 2; i < lines.length; i++) {
+            System.out.println(lines[i]);
+            String[] lineData = new String[8];
+            lineData[0] = lines[i].substring(21,27);
+            lineData[1] = lines[i].substring(27,35);
+            lineData[2] = lines[i].substring(35,40);
+            lineData[3] = lines[i].substring(40,45);
+            lineData[4] = lines[i].substring(45,54);
+            lineData[5] = lines[i].substring(54,63);
+            lineData[6] = lines[i].substring(63,68);
+            lineData[7] = lines[i].substring(68,74);
+            
+            //now strip spaces from each string
+            for (int j = 0; j < lineData.length; j++) {
+                lineData[j] = lineData[j].replaceAll(" ","");
+                System.out.print("line " + j + " |" + lineData[j] + "| ");
+            }
+            System.out.println();
+
+            planeData[i-2] = lineData;
+        }
+
+        return planeData;
+    }
 
     public static void main(String args[]) throws Exception{
         String flightWebpageText = getWebPage(flightDataUrl);
+        String[][] flightData    = getFlights(flightWebpageText);
 
         System.out.println("\nFull Webpage\nxxxxxxxxxxxxxxxxx\n"+flightWebpageText+"\nxxxxxxxxxxxxxxxxx");
+        System.out.println("Flight data");
+        for (int i = 0; i < flightData.length; i++) {
+            System.out.println("\n\nFlight " + i + "\n\n");
+            for (int j = 0; j < flightData[i].length; j++) {
+                System.out.println("    At index " + j + ": " + flightData[i][j]);
+            }
+        }
     }
 }
